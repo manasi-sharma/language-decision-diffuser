3e52eb2e18e49ee5282909d9374566d9c434f870
Manasi Sharma <45543113+manasi-sharma@users.noreply.github.com>
2023-03-14 00:40:05 -0700

    update

--- code/diffuser/datasets/normalization.py
+++ code/diffuser/datasets/normalization.py
@@ -54,9 +54,9 @@ def flatten(dataset, path_lengths):
         length = 32
         flattened[key] = np.concatenate([
             x[:length]
-            for x, _ in zip(xs, [])
+            #for x, length in zip(xs, path_lengths)
+            for x in xs
         ], axis=0)
-        #for x, length in zip(xs, path_lengths)
     return flattened
 
 #-----------------------------------------------------------------------------#
