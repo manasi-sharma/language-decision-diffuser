59932a1a439e223acc629260be4b04609bd6df53
Manasi Sharma <45543113+manasi-sharma@users.noreply.github.com>
2023-03-13 23:26:35 -0700

    update

--- code/analysis/train.py
+++ code/analysis/train.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.append('/iliad/u/manasis/language-decision-diffuser/code/')
 if __name__ == '__main__':
     from ml_logger import logger, instr, needs_relaunch
     from analysis import RUN
